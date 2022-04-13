5bf66768130a78d9991eb06989d4edd42b6f6ff1
TR742 <Tyrich1442@gmail.com>
2022-04-12 22:26:40 -0700

    Quest Arr created/init inquirer added

--- Develop/index.js
+++ Develop/index.js
@@ -2,15 +2,48 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    
-}];
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter a title for the project',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description for the project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please describe the installation process for the project',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Please describe how to use the project',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please include the contributors for the project',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'Please describe how to test the project',
+        name: 'test'
+    }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (input) {
+        console.log(input);
+    })
+};
 
 // Function call to initialize app
 init();
