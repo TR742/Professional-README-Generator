45486e7a93f982e920da0fcc337293cee08191f5
TR742 <Tyrich1442@gmail.com>
2022-04-14 20:23:23 -0700

    render badge function works

--- Develop/README.md
+++ Develop/README.md
@@ -1,13 +1,16 @@
-# jf
+# f
     
-    jf
+    f
 
-    jf
+    f
 
-    fj
+    f
 
-    jf
+    f
 
-    jf
+    f
 
-    MIT
+    Boost
+
+    [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+    
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Modzilla') {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+  } else if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else {
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -25,6 +35,9 @@ function generateMarkdown(data) {
     ${data.test}
 
     ${data.license}
+
+    ${renderLicenseBadge(data.license[0])}
+    
 `;
 }
 
